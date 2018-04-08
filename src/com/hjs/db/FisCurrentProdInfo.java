0bf4137260b0526c3cf3d9223db2d53715d0c628
huangxiao <huangxiao@hdfax.com>
2018-04-08 14:30:21 +0800

    新增活期产品信息类

---
+++ src/com/hjs/db/FisCurrentProdInfo.java
@@ -0,0 +1,22 @@
+package com.hjs.db;
+/**
+* @author huangxiao
+* @version 创建时间：2018年3月8日 上午11:16:49
+* 类说明
+*/
+public class FisCurrentProdInfo {
+	private String id;
+	private String product_name;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+}
