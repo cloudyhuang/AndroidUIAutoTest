e576184c435cdd6b5b6718bdd3de523fc82769c8
huangxiao <huangxiao@hdfax.com>
2018-04-08 14:31:31 +0800

    新增获取活期产品ID的sql接口

--- src/com/hjs/mybatis/inter/EifFisOperation.java
+++ src/com/hjs/mybatis/inter/EifFisOperation.java
@@ -1,8 +1,10 @@
 package com.hjs.mybatis.inter;
 
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import com.hjs.db.FisCollectPlan;
+import com.hjs.db.FisCurrentProdInfo;
 import com.hjs.db.FisHonorOrder;
 import com.hjs.db.FisProdInfo;
 import com.hjs.db.FisSecMarketProd;
@@ -10,7 +12,9 @@ import com.hjs.db.FisSecMarketProd;
 
 public interface EifFisOperation {
 	public FisProdInfo getFisProdInfo(String product_name);
-
+	@Select("SELECT * from t_fis_current_prod_info where product_name=#{product_name}")
+	public FisCurrentProdInfo getFisCurrentProdInfo(String product_name);
+	
 	public FisCollectPlan getFisCollectPlan(String assets_id);
 	@Update("UPDATE t_fis_honor_order set prod_inception_date=#{prod_inception_date} where product_name=#{product_name}")
 	public int setHonorOrderInceptionDate(FisHonorOrder fisHonorOrder);
